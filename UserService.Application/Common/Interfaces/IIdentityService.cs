14e3272d39a9ca71461b6de6189055e95f80a919
Denys Sychov <denys.sychov.dev@gmail.com>
2025-09-19 14:22:46 +0100

    remove identity service

--- UserService.Application/Common/Interfaces/IIdentityService.cs
+++
@@ -1,9 +0,0 @@
-﻿using UserService.Domain.Entities;
-
-namespace UserService.Application.Common.Interfaces;
-
-public interface IIdentityService
-{
-    Task<ApplicationUser> RegisterAsync(string email, string password, string fullName);
-    Task<ApplicationUser?> LoginAsync(string email, string password);
-}
