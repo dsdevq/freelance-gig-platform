ccd0ceda36a116542f25a895193cf11cc606ba04
Denys Sychov <denys.sychov.dev@gmail.com>
2025-10-02 18:33:02 +0100

    working scenario of authentication and authorization

--- UserService.API/Endpoints/UserEndpoints.cs
+++ UserService.API/Endpoints/UserEndpoints.cs
@@ -9,17 +9,17 @@ public static class UserEndpoints
 {
     public static void MapUserEndpoints(this IEndpointRouteBuilder app)
     {
-        app.MapGet(UserRoutes.ClientMe, [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        app.MapGet(UserRoutes.ClientMe,
             async () =>
             {
                 return Results.Ok("This is a client me");
-            });
+            }).RequireAuthorization("client-policy");
         
         app.MapGet(UserRoutes.FreelancerMe, () =>
     {
             return Results.Ok();
         }).RequireAuthorization(policy => policy.RequireRole(Roles.Freelancer));
 
-
+    
     }
 }
--- UserService.API/Program.cs
+++ UserService.API/Program.cs
@@ -1,12 +1,15 @@
+using System.Security.Claims;
+using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 using UserService.API.Endpoints;
 using UserService.API.Extensions;
 using UserService.API.Handlers;
 using UserService.API.OptionsSetup;
 using UserService.Application;
-using UserService.Domain.Constants;
 using UserService.Infrastructure;
 using UserService.Infrastructure.Extensions;
+using UserService.Infrastructure.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,19 +18,34 @@ builder.Services.AddApplication();
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
-builder.Services.AddAuthorizationBuilder()
-    .AddPolicy(Roles.Client, policy =>
-        policy
-            .RequireRole(Roles.Client)
-            .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme));
-
-builder.Services.AddSwagger(); // <- your extension
-
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer();
-
 builder.Services.ConfigureOptions<JwtOptionsSetup>();
 builder.Services.ConfigureOptions<JwtBearerOptionsSetup>();
+
+builder.Services.AddAuthentication(x =>
+    {
+        x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    })
+    .AddJwtBearer(x =>
+    {
+        x.TokenValidationParameters = new()
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            RoleClaimType = ClaimTypes.Role,
+            NameClaimType = ClaimTypes.Name,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = builder.Configuration["JwtSettings:Issuer"],
+            ValidAudience = builder.Configuration["JwtSettings:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(builder.Configuration["JwtSettings:SecretKey"]))
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+builder.Services.AddSwagger(); // <- your extension
 
 var app = builder.Build();
 
@@ -37,14 +55,21 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerWithUI(); // <- your extension
 }
 
-app.UseHttpsRedirection();
-app.UseExceptionHandler();
-app.UseAuthentication();
-app.UseAuthorization();
 
 // Endpoints
 app.MapAuthEndpoints();
 app.MapUserEndpoints();
+
+app.MapGet("me", (ClaimsPrincipal claimsPrincipal) =>
+{
+    return Results.Ok("Result it okay");
+    // return Results.Ok(claimsPrincipal.Claims.ToDictionary((c) => c.Type, (c) => c.Value));
+}).RequireAuthorization();
+
+app.UseHttpsRedirection();
+app.UseExceptionHandler();
+app.UseAuthentication();
+app.UseAuthorization();
 
 // Database
 app.ApplyMigrations();
