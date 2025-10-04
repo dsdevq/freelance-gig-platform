d425144d36249a33da378a816da0c7144c19d651
Denys Sychov <denys.sychov.dev@gmail.com>
2025-10-04 09:57:53 +0100

    replace db context usage with unit of work

--- UserService.Infrastructure/Identity/IdentityService.cs
+++ UserService.Infrastructure/Identity/IdentityService.cs
@@ -8,11 +8,10 @@ using UserService.Infrastructure.Entities;
 
 namespace UserService.Infrastructure.Identity;
 
-public class IdentityService(
-    UserManager<AppIdentityUser> userManager,
+public class IdentityService(UserManager<AppIdentityUser> userManager,
     SignInManager<AppIdentityUser> signInManager,
-    UserDbContext userDbContext
-) : IIdentityService
+    IUnitOfWork unitOfWork
+    ) : IIdentityService
 {
     public async Task<UserModel> SignInAsync(SignInModel model)
     {
@@ -20,15 +19,13 @@ public class IdentityService(
         if (user is null) 
             throw new LoginFailedException("Invalid email or password");
 
-        var result = await signInManager.CheckPasswordSignInAsync(
-            user, model.Password, lockoutOnFailure: false);
-
+        var result = await signInManager.CheckPasswordSignInAsync(user, model.Password, lockoutOnFailure: false);
         if (!result.Succeeded)
             throw new LoginFailedException("Invalid email or password");
-
+        
         var roles = await userManager.GetRolesAsync(user);
-        var parsedRole = ParseRole(roles);
-
+        var parsedRole = Enum.Parse<RoleType>(roles[0]);
+        
         return new UserModel(user.Email!, user.UserName!, parsedRole)
         {
             Id = user.Id
@@ -37,59 +34,63 @@ public class IdentityService(
 
     public async Task<UserModel> SignUpAsync(SignUpModel model, RoleType roleName)
     {
-        await using var transaction = await userDbContext.Database.BeginTransactionAsync();
+        await unitOfWork.BeginTransactionAsync();
 
-        var user = new AppIdentityUser
+        try
         {
-            Email = model.Email,
-            UserName = model.Name
-        };
+            var user = new AppIdentityUser
+            {
+                Email = model.Email,
+                UserName = model.Name
+            };
 
-        var createUser = await userManager.CreateAsync(user, model.Password);
-        if (!createUser.Succeeded)
-        {
-            await transaction.RollbackAsync();
-            throw new SignUpFailedException(string.Join("; ", createUser.Errors.Select(e => e.Description)));
+            var createUser = await userManager.CreateAsync(user, model.Password);
+            if (!createUser.Succeeded)
+                throw new SignUpFailedException(string.Join("; ", createUser.Errors.Select(e => e.Description)));
+
+            var assignRoleResult = await userManager.AddToRoleAsync(user, roleName.ToString());
+            if (!assignRoleResult.Succeeded)
+                throw new SignUpFailedException(string.Join("; ", assignRoleResult.Errors.Select(e => e.Description)));
+
+            await unitOfWork.CommitTransactionAsync();
+
+            var roles = await userManager.GetRolesAsync(user);
+            var parsedRole = Enum.Parse<RoleType>(roles.First());
+
+            return new UserModel(user.Email, user.UserName, parsedRole)
+            {
+                Id = user.Id
+            };
         }
-
-        var assignRoleResult = await userManager.AddToRoleAsync(user, roleName.ToString());
-        if (!assignRoleResult.Succeeded)
+        catch
         {
-            await transaction.RollbackAsync();
-            throw new SignUpFailedException(string.Join("; ", assignRoleResult.Errors.Select(e => e.Description)));
+            await unitOfWork.RollbackTransactionAsync();
+            throw;
         }
-
-        await transaction.CommitAsync();
-
-        var roles = await userManager.GetRolesAsync(user);
-        var parsedRole = ParseRole(roles);
-
-        return new UserModel(user.Email!, user.UserName!, parsedRole)
-        {
-            Id = user.Id
-        };
     }
-
+    
     public async Task<UserModel> GetUserByIdAsync(Guid userId, CancellationToken cancellationToken)
     {
         var user = await userManager.FindByIdAsync(userId.ToString());
+
+        
         if (user is null)
+        {
             throw new UnauthorizedAccessException("User not found");
+        }
 
         var roles = await userManager.GetRolesAsync(user);
-        if (!roles.Any())
-            throw new UnauthorizedAccessException("User not found");
 
-        var parsedRole = ParseRole(roles);
+        if (!roles.Any())
+        {
+            throw new UnauthorizedAccessException("User not found");
+        }
+
+        var parsedRole = Enum.Parse<RoleType>(roles.First());
 
         return new UserModel(user.Email!, user.UserName!, parsedRole)
         {
             Id = user.Id
         };
-    }
-
-    private static RoleType ParseRole(IList<string> roles)
-    {
-        return Enum.Parse<RoleType>(roles.First());
     }
 }
