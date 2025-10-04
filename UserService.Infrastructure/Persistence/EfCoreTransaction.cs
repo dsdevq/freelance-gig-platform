5f3b8a57c7fec40b79c1ee626e6cbb2664654ba7
Denys Sychov <denys.sychov.dev@gmail.com>
2025-10-04 16:45:29 +0100

    replace with primary constructor

--- UserService.Infrastructure/Persistence/EfCoreTransaction.cs
+++ UserService.Infrastructure/Persistence/EfCoreTransaction.cs
@@ -3,21 +3,14 @@ using UserService.Application.Common.Interfaces;
 
 namespace UserService.Infrastructure.Persistence;
 
-public class EfCoreTransaction : ITransaction
+public class EfCoreTransaction(IDbContextTransaction efTransaction) : ITransaction
 {
-    private readonly IDbContextTransaction _efTransaction;
-
-    public EfCoreTransaction(IDbContextTransaction efTransaction)
-    {
-        _efTransaction = efTransaction;
-    }
-
     public async Task CommitAsync(CancellationToken cancellationToken = default) 
-        => await _efTransaction.CommitAsync(cancellationToken);
+        => await efTransaction.CommitAsync(cancellationToken);
 
     public async Task RollbackAsync(CancellationToken cancellationToken = default) 
-        => await _efTransaction.RollbackAsync(cancellationToken);
+        => await efTransaction.RollbackAsync(cancellationToken);
 
     public async ValueTask DisposeAsync() 
-        => await _efTransaction.DisposeAsync();
+        => await efTransaction.DisposeAsync();
 }
